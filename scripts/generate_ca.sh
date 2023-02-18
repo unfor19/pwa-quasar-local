91a3fc806ca344c2074a51a8a245087ee6cd7f3e
Meir Gabay <unfor19@gmail.com>
2023-02-18 12:39:54 +0200

    Update generate_ca.sh

--- scripts/generate_ca.sh
+++ scripts/generate_ca.sh
@@ -1,47 +1,74 @@
 #!/usr/bin/env bash
 
+# Name: generate_self_signed_ca_certificate.sh
+# Description: Generate a self-signed CA rootKey, rootCA, certificate per domain (CNAME) for both pem and DER formats
+# Author: Meir Gabay (unfor19)
+
 set -e
 set -o pipefail
 
-# Requires - openssl
-# Based on: https://android.stackexchange.com/a/238859/363870
+
+### Functions
+msg_error(){
+    local msg="$1"
+    echo -e "[ERROR] $(date) :: $msg"
+    exit 1
+}
+
+
+msg_log(){
+    local msg="$1"
+    echo -e "[LOG] $(date) :: $msg"
+}
 
 # IMPORTANT: choose ".test" as a suffix
 # ".dev" and ".local" are reserved
 _DOMAIN="${1:-"$DOMAIN"}"
 _SUFFIX="${SUFFIX:-"test"}"
 if [[ -z "$_DOMAIN" ]]; then
-    echo "[ERR] Required variable: DOMAIN"
+  msg_error "Required variable: DOMAIN"
 fi
 _FQDN="${FQDN:-"${_DOMAIN}.${_SUFFIX}"}"
 
 
-_CERTS_DIR_PATH="${CERTS_DIR_PATH:-"awesome-pwa/.certs"}"
+_CERTS_DIR_PATH="${CERTS_DIR_PATH:-".certs"}"
+msg_log "Creating the directory ${_CERTS_DIR_PATH} ..."
 mkdir -p "$_CERTS_DIR_PATH"
 pushd "$_CERTS_DIR_PATH" || exit
 
-
 _SKIP_ROOTCA_KEY="${SKIP_ROOT_CA_KEY:-"false"}"
 _ROOTCA_KEY_PATH="${ROOTCA_KEY_PATH:-"rootCA.key"}"
-_ROOTCA_PEM_PATH="${CERT_OUT_PATH:-"rootCA.pem"}"
+_ROOTCA_PEM_PATH="${ROOTCA_PEM_PATH:-"rootCA.pem"}"
 _ROOTCA_CERT_EXPIRE_DAYS="${ROOTCA_CERT_EXPIRE_DAYS:-"3650"}"
 _X509V3_CONFIG_PATH="${X509V3_CONFIG_PATH:-"x509v3_config.ext"}"
-
 
 _DOMAIN_CRT_PATH="${DOMAIN_CRT_PATH:-"${_FQDN}.crt"}"
 _DOMAIN_CRT_DER_PATH="${DOMAIN_CRT_DER_PATH:-"${_FQDN}.der.crt"}"
 _DOMAIN_CERT_EXPIRE_DAYS="${DOMAIN_CERT_EXPIRE_DAYS:-"3650"}"
 
+msg_log "Print values:"
+echo "
+############################################################
+         CERTS_DIR_PATH: '${_CERTS_DIR_PATH}'
+       SKIP_ROOT_CA_KEY: '${_SKIP_ROOTCA_KEY}'
+        ROOTCA_KEY_PATH: '${_ROOTCA_KEY_PATH}'
+        ROOTCA_PEM_PATH: '${_ROOTCA_PEM_PATH}'
+ROOTCA_CERT_EXPIRE_DAYS: '${_ROOTCA_CERT_EXPIRE_DAYS}'
+     X509V3_CONFIG_PATH: '${_X509V3_CONFIG_PATH}'
+        DOMAIN_CRT_PATH: '${_DOMAIN_CRT_PATH}'
+############################################################
+"
+
 ### Root CA
 
 if [[ "$_SKIP_ROOTCA_KEY" != "true" ]]; then
   if [[ ! -f "$_ROOTCA_KEY_PATH" ]]; then
-    echo "Generating private key for rootCA"
+    msg_log "Generating private key for rootCA"
     # 2048 bit key is hardcoded no purpose - https://expeditedsecurity.com/blog/measuring-ssl-rsa-keys/
     openssl genrsa -out "$_ROOTCA_KEY_PATH" 2048
   fi
   if [[ ! -f "$_ROOTCA_PEM_PATH" ]]; then
-    echo "Generating the rootCA Certificate ${_ROOTCA_PEM_PATH} and signing it with the private key ${_ROOTCA_KEY_PATH}"
+    msg_log "Generating the rootCA Certificate ${_ROOTCA_PEM_PATH} and signing it with the private key ${_ROOTCA_KEY_PATH}"
     openssl req -new \
     -days "$_ROOTCA_CERT_EXPIRE_DAYS" \
     -key "$_ROOTCA_KEY_PATH" \
@@ -56,18 +83,19 @@ fi
 # https://www.openssl.org/docs/manmaster/man5/x509v3_config.html
 cat > "$_X509V3_CONFIG_PATH" << EOF
 authorityKeyIdentifier=keyid,issuer
-basicConstraints=CA:true
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-subjectAltName = @alt_names
+basicConstraints=critical,CA:true
+keyUsage=critical,digitalSignature,nonRepudiation,cRLSign,keyCertSign
+subjectAltName=@alt_names
+issuerAltName=issuer:copy
+subjectKeyIdentifier=hash
 [alt_names]
-DNS.1 = ${_FQDN}
+DNS.1=${_FQDN}
 EOF
 
 
-echo "Applicant generates CSR with ${_ROOTCA_PEM_PATH}, though no CSR file will be generated"
-echo "rootCA signs the certifcate with ${_ROOTCA_KEY_PATH}"
-echo "rootCA generates the CA certificate ${_DOMAIN_CRT_PATH}"
-# -extfile is for Android, as x509 needs to be CA:true and include DNS name
+msg_log "Applicant generates CSR with ${_ROOTCA_PEM_PATH}, though no CSR file will be generated"
+msg_log "rootCA signs the certifcate with ${_ROOTCA_KEY_PATH}"
+msg_log "rootCA generates the CA certificate ${_DOMAIN_CRT_PATH}"
 openssl x509 -req \
   -days "$_ROOTCA_CERT_EXPIRE_DAYS" \
   -in "$_ROOTCA_PEM_PATH" \
@@ -76,8 +104,8 @@ openssl x509 -req \
   -out "$_DOMAIN_CRT_PATH"
 
 
-# Convert the 'crt' certificate to DER format
-echo "Converting ${_DOMAIN_CRT_DER_PATH} to DER format for ${_FQDN}"
+# Convert the 'crt' certificate to DER format for Android
+msg_log "Converting ${_DOMAIN_CRT_DER_PATH} to DER format for ${_FQDN}"
 openssl x509 -inform PEM -outform DER -in "$_DOMAIN_CRT_PATH" -out "$_DOMAIN_CRT_DER_PATH"
 
 echo "
@@ -91,39 +119,43 @@ echo "
 
 ### Output Files
 
-           rootCA Certificate: ${_ROOTCA_PEM_PATH}
-           rootCA Private Key: ${_ROOTCA_KEY_PATH}
- Local Machine CA Certificate: ${_DOMAIN_CRT_PATH}    
-Android Device CA Certificate: ${_DOMAIN_CRT_DER_PATH}
+           rootCA Certificate: ${_CERTS_DIR_PATH}/${_ROOTCA_PEM_PATH}
+           rootCA Private Key: ${_CERTS_DIR_PATH}/${_ROOTCA_KEY_PATH}
+ Local Machine CA Certificate: ${_CERTS_DIR_PATH}/${_DOMAIN_CRT_PATH}    
+Android Device CA Certificate: ${_CERTS_DIR_PATH}/${_DOMAIN_CRT_DER_PATH}
 
 
 ### Use in quasar.config.js
 
 devServer: {
   https: {
-    cert: '${_DOMAIN_CRT_PATH}',
-    key: '${_ROOTCA_KEY_PATH}',
+    cert: '${_CERTS_DIR_PATH}/${_DOMAIN_CRT_PATH}',
+    key: '${_CERTS_DIR_PATH}/${_ROOTCA_KEY_PATH}',
   },
   port: 443,
   open: false
 },
 
 
-### Install ${_DOMAIN_CRT_PATH} certificate on local machine
+### Install the certificate '${_DOMAIN_CRT_PATH}' on local machine
 
 - macOS
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain \"${_CERTS_DIR_PATH}/${_DOMAIN_CRT_PATH}\"
 
 
-### Install ${_DOMAIN_CRT_DER_PATH} certificate on Android device    
+### Install the certificate '${_DOMAIN_CRT_DER_PATH}' on Android device    
 
-Local Machine > Upload ${_DOMAIN_CRT_DER_PATH} to Google Drive
-On Android    > Download ${_DOMAIN_CRT_DER_PATH} to local storage
-On Android    > Settings > CA Certificate > Install downloaded certificate - \"${_DOMAIN_CRT_DER_PATH}\"
+Local Machine > Upload '${_DOMAIN_CRT_DER_PATH}' to Google Drive
+On Android    > Download '${_DOMAIN_CRT_DER_PATH}' to local storage
+On Android    > Settings > CA Certificate > Install downloaded certificate - '${_DOMAIN_CRT_DER_PATH}'
 
 Why DER? See https://knowledge.digicert.com/quovadis/ssl-certificates/ssl-general-topics/what-is-der-format.html
 Quoting: \"..DER is often used with Java platforms.\" 
 Android is based on Java, so it makes sense to use DER
+
+### Lint and verify certificate
+
+Lint the output certificate '${_DOMAIN_CRT_PATH}' by copy-pasting its value to - https://crt.sh/lintcert
 "
 
 popd || exit
